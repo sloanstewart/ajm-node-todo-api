351aa13e03215525e12c940a4a48525515b8547a
sloanstewart <sloanstewartdev@gmail.com>
2017-11-08 20:51:37 -0500

    Setup test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var promise = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
+var promise = mongoose.connect(process.env.MONGODB_URI, {
     useMongoClient: true
 });
 
