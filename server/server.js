668e2e274aa8545f9dfda6973f0b7b6a408ca309
sloanstewart <sloanstewartdev@gmail.com>
2017-11-15 06:21:48 -0500

    Add POSt /users route and token method

--- server/server.js
+++ server/server.js
@@ -98,8 +98,10 @@ app.post('/users', (req, res) => {
     console.log(body);
     var user = new User(body);
 
-    user.save().then((user)=>{
-        res.send(user);
+    user.save().then(()=>{
+       return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
     }).catch((err)=>{
         res.status(400).send(err);
     });
