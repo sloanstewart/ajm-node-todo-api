b57e642fec2f3ad56cf849047c644c568d1cf06d
sloanstewart <sloanstewartdev@gmail.com>
2017-11-15 05:13:22 -0500

    hashing

--- server/server.js
+++ server/server.js
@@ -92,6 +92,19 @@ app.patch('/todos/:id', (req, res) => {
     })
 });
 
+// USERS =====================
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    console.log(body);
+    var user = new User(body);
+
+    user.save().then((user)=>{
+        res.send(user);
+    }).catch((err)=>{
+        res.status(400).send(err);
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
