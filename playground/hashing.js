b989144a5505cb762792f620788c029cb5f24e61
sloanstewart <sloanstewartdev@gmail.com>
2017-11-15 05:13:33 -0500

    hashing

---
+++ playground/hashing.js
@@ -0,0 +1,39 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+    id: 10
+};
+
+var token = jwt.sign(data, 'supersecret');
+console.log(token);
+
+var decoded = jwt.verify(token, 'supersecret');
+console.log('decoded', decoded);
+
+// var message = 'Yo im a user!';
+// var hash = SHA256(message).toString();
+
+// console.log(`
+//     Messsage: ${message}
+//     Hash: ${hash}
+// `);
+
+// var data = {
+//   id: 4
+// };
+
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'supersecret').toString()
+// };
+
+// // token.data.id = 5;
+// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'supersecret').toString();
+// if (resultHash === token.hash) {
+//     console.log('Data was not changed');
+// } else {
+//     console.log('Data was changed. No good!');
+// }
